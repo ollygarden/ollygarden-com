e6b3f3f84c54ceb34dcac3148fcc57856cb8134e
Juraci Paixão Kröhling <juraci@kroehling.de>
2025-05-20 15:34:43 +0200

    feat: privacy policy and terms of use (#89)
    Signed-off-by: Juraci Paixão Kröhling <juraci@kroehling.de>

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
@@ -11,6 +10,8 @@ import NotFound from "./pages/NotFound";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { PostHogProvider } from "./providers/PostHogProvider";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import TermsOfUse from "./pages/TermsOfUse";
 
 const queryClient = new QueryClient();
 
@@ -28,6 +29,8 @@ const App = () => (
                 <Route path="/" element={<Index />} />
                 <Route path="/careers" element={<Careers />} />
                 <Route path="/careers/:id" element={<JobDetail />} />
+                <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+                <Route path="/terms-of-use" element={<TermsOfUse />} />
                 <Route path="*" element={<NotFound />} />
               </Routes>
             </main>
